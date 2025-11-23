582ec9b2b7ce43f16cfb305bdd069b86b10f854f
robkebab <rpc219@gmail.com>
2025-11-23 13:29:40 -0600

    better example using sleep

--- streaming/workflows/status/index.ts
+++ streaming/workflows/status/index.ts
@@ -1,10 +1,13 @@
 import { closeWritable, updateStatus } from "./steps";
+import { sleep } from "workflow";
 
 export async function statusWorkflow() {
   "use workflow";
 
-  await updateStatus("Requested", 2000);
-  await updateStatus("In Progress", 15000);
+  await updateStatus("Requested");
+  await sleep(2000);
+  await updateStatus("In Progress");
+  await sleep(15000);
   await updateStatus("Completed");
   await closeWritable();
 }
